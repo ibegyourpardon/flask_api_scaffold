efafccd03f9f1d397a5cca0eb855fc921e6917fd
feiandxs <yiziyint@gmail.com>
2020-08-18 17:30:28 +0800

    fixed:added logger

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
 
+
+def create_logger():
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    handler = TimedRotatingFileHandler('logs/fas.log', when='D')
+    logging_format = logging.Formatter(
+        '[%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - line:%(lineno)s]: %(message)s')
+    handler.setFormatter(logging_format)
+    return handler
 
 app = Flask(__name__)
+app.logger.addHandler(create_logger())
 
 from yo import test1
